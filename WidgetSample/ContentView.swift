39102cedca74a5bf68f3a3703e4a811887639aca
d1v1b <koba.reiji+github@gmail.com>
2020-06-13 21:45:47 +0900

    Initial Commit

---
+++ WidgetSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WidgetSample
+//
+//  Created by SoNice! on 2020/06/13.
+//  Copyright © 2020 SoNice!. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
